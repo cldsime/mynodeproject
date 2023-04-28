87fc1b74b86278fdc61ee7282eafdf825a072c46
Woldesenbet <sfw@b0be83750baa.ant.amazon.com>
2023-04-28 08:56:48 -0700

    updated my app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the home page!")
+    res.send("Welcome to our home page, Netta!https://aws.amazon.com/")
 });
 
 const port = process.env.port || 3001; 
